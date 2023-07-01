a9c403c29b4cec6b7037922dd7cff3e5ca93b01b
Eka Maulana Badarin <45058998+maulanabadarin123@users.noreply.github.com>
2023-07-01 16:56:57 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
-import { fetchIpAddress } from "@/utils";
 import Link from "next/link";
 
 export default async function Home() {
-  const searchIp = await fetchIpAddress();
-  console.log(searchIp)
+  
 
   return (
     <main className="mx-auto flex items-center justify-center h-screen">
       <div className="border-2 p-4 border-slate-900">
-        <h1 className="text-4xl text-red-700">IP Address : {searchIp.ipString}</h1>
+        <h1 className="text-4xl text-red-700">IP Address : 192.168.100.32</h1>
         <Link
           href="/ip"
           className="border-4 bg-cyan-500 text-white p-2 mt-3 flex justify-center text-xl"
