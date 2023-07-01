481e092cd4a74c7121bf6ad4c999243f20f904c4
Eka Maulana Badarin <45058998+maulanabadarin123@users.noreply.github.com>
2023-07-01 16:57:49 +0700

    Update page.js

--- app/ip/page.js
+++ app/ip/page.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { fetchIpAddress } from "@/utils";
+
 
 export default async function page() {
-  const searchIp = await fetchIpAddress();
+ 
 
   return (
     <section className="mx-auto flex items-center justify-center h-screen">
       <div className="border-4 p-4 border-slate-900">
-        <h1 className="text-4xl text-red-700">My IP : {searchIp.ipString}</h1>
+        <h1 className="text-4xl text-red-700">My IP : 192.168.100.32</h1>
       </div>
     </section>
   );
